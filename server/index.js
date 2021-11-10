039234ff28b4e10126da89f54fd2ed57b41adc41
mAtencio27 <mAtencio27@hotmail.com>
2021-11-10 21:04:59 +0900

    made changes from merge conflicts

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 
-<<<<<<< HEAD
-const PORT = process.env.PORT || 9000;
-=======
 const PORT = process.env.PORT || 8080;
->>>>>>> a3702605dda4af30558c8ece17c6cbab498d88c1
 
 app.listen(PORT, () => {
     console.log(`App Listening on PORT${PORT}!`);
