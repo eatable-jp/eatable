28cd228025bbb0871edb3c3ba405e2d3b27c0f5d
SumitVermakgp <sumitverma9628@gmail.com>
2021-11-11 23:30:06 +0900

    connection works

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cors = require("cors")
 const knex = require('knex')
 
-const environment = process.env.ENVIRONMENT || 'development';
-const config = require("../knexfile")[environment];
-const db = knex(config);
+//const environment = process.env.ENV || 'development';
+//const config = require("../knexfile")[environment];
+//const db = knex(config);
 
 const app = express()
 
@@ -15,17 +16,178 @@ app.use(cors({
     origin: '*'
 }));
 
+// Automatically parse request body as form data.
+app.use(express.urlencoded({extended: false}));
+
+app.enable('trust proxy');
+
 //Serve the static assets
 app.use(express.static(path.resolve(__dirname,"..","build")));
 
-//test endpoint
-app.get("/hello", async(req,res) => {
-    res.json("Let's save some food!!!!!!!!!!")
-})
-/******************************************************************************** */
-/******************************************************************************** */
-/******************************************************************************** */
-/******************************************************************************** */
+// Set Content-Type for all responses for these routes.
+app.use((req, res, next) => {
+    res.set('Content-Type', 'text/html');
+    next();
+});
+
+
+
+// Set up a variable to hold our connection pool. It would be safe to
+// initialize this right away, but we defer its instantiation to ease
+// testing different configurations.
+let pool;
+
+app.use(async (req, res, next) => {
+  if (pool) {
+    return next();
+  }
+  try {
+    pool = await createPoolAndEnsureSchema();
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/*
+const createUnixSocketPool = async config => {
+    const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+  
+    // Establish a connection to the database
+    return knex({
+      client: 'pg',
+      connection: {
+        user: process.env.DB_USER, 
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+      },
+      // ... Specify additional properties here.
+      ...config,
+    });
+};
+*/
+
+const createTcpPool = async config => {
+    // Extract host and port from socket address
+    const dbSocketAddr = process.env.DB_HOST.split(':'); // e.g. '127.0.0.1:5432'
+  
+    // Establish a connection to the database
+    return knex({
+      client: 'pg',
+      connection: {
+        user: process.env.DB_USER, // e.g. 'my-user'
+        password: process.env.DB_PASS, // e.g. 'my-user-password'
+        database: process.env.DB_NAME, // e.g. 'my-database'
+        host: dbSocketAddr[0], // e.g. '127.0.0.1'
+        port: dbSocketAddr[1], // e.g. '5432'
+      },
+      // ... Specify additional properties here.
+      ...config,
+    });
+  };
+
+const createPool = async () => {
+    // Configure which instance and what database user to connect with.
+    // Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    // something like https://cloud.google.com/kms/ to help keep secrets secret.
+    const config = {pool: {}};
+    config.pool.max = 5;
+    config.pool.min = 5;
+    config.pool.acquireTimeoutMillis = 60000;
+    config.pool.createTimeoutMillis = 30000;
+    config.pool.idleTimeoutMillis = 600000;
+    config.pool.createRetryIntervalMillis = 200;
+    console.log("this pool")
+
+    return createTcpPool(config);
+};
+
+/*
+
+const ensureSchema = async pool => {
+    const hasTable = await pool.schema.hasTable('votes');
+    if (!hasTable) {
+      return pool.schema.createTable('votes', table => {
+        table.increments('vote_id').primary();
+        table.timestamp('time_cast', 30).notNullable();
+        table.specificType('candidate', 'CHAR(6)').notNullable();
+      });
+    }
+    logger.info("Ensured that table 'votes' exists");
+};
+
+
+const ensureSchema = async pool => {
+    const hasTable = await pool.schema.hasTable('items');
+    if (!hasTable) {
+      return pool.schema.createTable('items', table => {
+        table.increments('vote_id').primary();
+        table.timestamp('time_cast', 30).notNullable();
+        table.specificType('candidate', 'CHAR(6)').notNullable();
+      });
+    }
+};
+*/
+
+const createPoolAndEnsureSchema = async () =>
+  await createPool()
+    .then(async pool => {
+      //await ensureSchema(pool);
+      return pool;
+    })
+    .catch(err => {
+      //logger.error(err);
+      throw err;
+    });
+
+/*
+const insertVote = async (pool, vote) => {
+    try {
+    return await pool('votes').insert(vote);
+    } catch (err) {
+    throw Error(err);
+    }
+};
+
+const getVotes = async pool => {
+    return await pool
+      .select('candidate', 'time_cast')
+      .from('votes')
+      .orderBy('time_cast', 'desc')
+      .limit(5);
+};
+
+const getVoteCount = async (pool, candidate) => {
+    return await pool('votes').count('vote_id').where('candidate', candidate);
+};
+
+*/
+
+const getItems = async pool => {
+    return await pool
+        .select('*')
+        .from('items')
+}
+
+app.get('/items', async (req, res) => {
+    pool = pool || (await createPoolAndEnsureSchema());
+    console.log("pool created ")
+    //console.log(pool)
+    try {
+        const items = await getItems(pool)
+        res.json(items);
+    } catch (err) {
+        console.error(err);
+    res
+      .status(500)
+      .send('Unable to load page; see logs for more details.')
+      .end();
+    }
+});
+
+
+/*
 //***************************************ITEMS*************************************
 app.get('/items', async(req,res) => {
     const items = await db.select().table("items");
@@ -64,11 +226,8 @@ app.delete('/items',async(req,res) => {
 
     res.send(`${foundItem[0].name} has been deleted`)
 })
-/******************************************************************************** */
-/******************************************************************************** */
-/******************************************************************************** */
-/******************************************************************************** */
-//************************SELLERS************************************************ */
+
+//************************SELLERS************************************************ 
 
 app.get('/sellers', async(req,res) => {
     const allSellers = await db.select().table("sellers")
@@ -111,11 +270,7 @@ app.delete('/sellers',async(req,res) => {
     res.send(`${foundItem[0].shop_name} has been deleted`)
 })
 
-/******************************************************************************** */
-/******************************************************************************** */
-/******************************************************************************** */
-/******************************************************************************** */
-/************************************BUYERS************************************** */
+/************************************BUYERS************************************** 
 
 app.get("/buyers",async(req,res) => {
     const allBuyers = await db.select().table('buyers')
@@ -155,6 +310,12 @@ app.delete('/buyers',async(req,res) => {
     res.json(`${foundBuyer[0].display_name} has been deleted`)
 })
 
+*/
+
+//test endpoint
+app.get("/hello", async(req,res) => {
+    res.json("Let's save some food!!!!!!!!!!")
+})
 
 
 app.get("*", (req,res) => {
