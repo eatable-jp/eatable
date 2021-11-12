fb182e501cb3aa690eed23ae9ec0354035613cb5
唐戸渉太 <shotakarato@shotakarato-2.local>
2021-11-12 16:20:42 +0900

    feat: connected form with redux state

--- src/components/SellerForm.js
+++ src/components/SellerForm.js
@@ -1,31 +1,71 @@
+import { useForm } from "react-hook-form";
+// redux
+import { useDispatch } from "react-redux";
+import { updateSellerInfo } from "../slice/sellerInfoSlice";
 // react router
 import { LinkContainer } from "react-router-bootstrap";
 // bootstrap
 import { Container, Form, Button } from "react-bootstrap";
 
 export default function SellerForm() {
+  // setup react form
+  const { register, handleSubmit, reset } = useForm();
+  // setup redux
+  const dispatch = useDispatch();
+
+  // function for updating user info
+  function sendSellerInfo(updatedInfo) {
+    // validation get rid of empty value
+    Object.keys(updatedInfo).forEach((info) => {
+      if (updatedInfo[info] === "" || updatedInfo[info] === null) {
+        delete updatedInfo[info];
+      }
+    });
+    dispatch(updateSellerInfo(updatedInfo));
+    reset();
+  }
   return (
     <Container className="w-25">
-      <Form>
+      <Form onSubmit={handleSubmit(sendSellerInfo)}>
         <Form.Group className="mb-3" controlId="formBasicShopName">
           <Form.Label>Shop name</Form.Label>
-          <Form.Control type="text" placeholder="Enter shop name" />
+          <Form.Control
+            type="text"
+            placeholder="Enter shop name"
+            {...register("shop_name")}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Address</Form.Label>
-          <Form.Control type="text" placeholder="Enter address" />
+          <Form.Control
+            type="text"
+            placeholder="Enter address"
+            {...register("shop_location")}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
           <Form.Label>Phone number</Form.Label>
-          <Form.Control type="text" placeholder="Enter phone number" />
+          <Form.Control
+            type="text"
+            placeholder="Enter phone number"
+            {...register("phone_number")}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicOpeningTime">
           <Form.Label>Opening time</Form.Label>
-          <Form.Control type="text" placeholder="Enter opening time" />
+          <Form.Control
+            type="text"
+            placeholder="Enter opening time"
+            {...register("opening_time")}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicClosingTime">
           <Form.Label>Closing time</Form.Label>
-          <Form.Control type="text" placeholder="Enter closing time" />
+          <Form.Control
+            type="text"
+            placeholder="Enter closing time"
+            {...register("closing_time")}
+          />
         </Form.Group>
         <Button className="mr-2" variant="outline-success" type="submit">
           Submit
--- src/components/SellerProfile.js
+++ src/components/SellerProfile.js
@@ -1,39 +1,14 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
 // redux
-import { useSelector, useDispatch } from "react-redux";
-import { updateSellerInfo } from "../slice/sellerInfoSlice";
+import { useSelector } from "react-redux";
 // react router
 import { LinkContainer } from "react-router-bootstrap";
 // bootstrap
-import { Form, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export default function SellerProfile() {
   // setup redux
-  const dispatch = useDispatch();
   const sellerInfo = useSelector((state) => state.sellerInfo);
-
-  // setup react form
-  const { register, handleSubmit, reset } = useForm();
-  // function for updating user info
-  function sendSellerInfo(updatedInfo) {
-    // validation get rid of empty value
-    Object.keys(updatedInfo).forEach((info) => {
-      if (updatedInfo[info] === "" || updatedInfo[info] === null) {
-        delete updatedInfo[info];
-      }
-    });
-    dispatch(updateSellerInfo(updatedInfo));
-    reset();
-    toggleForm();
-  }
-
-  // formDisplay state
-  const [formDisplay, setFormDisplay] = useState(false);
-  // formDisplay state: function for toggling form
-  function toggleForm() {
-    setFormDisplay(!formDisplay);
-  }
   return (
     <>
       <div className="buyer-profile">
@@ -60,52 +35,6 @@ export default function SellerProfile() {
           <Button>Edit profile</Button>
         </LinkContainer>
       </div>
-      {/* form for updating user info */}
-      <form
-        className={formDisplay ? "update-form" : "update-form hidden"}
-        onSubmit={handleSubmit(sendSellerInfo)}
-      >
-        <fieldset className="update-form-fieldset">
-          <label className="update-form-label">
-            <span>Shop name</span>
-            <input type="text" name="shop_name" {...register("shop_name")} />
-          </label>
-          <label className="update-form-label">
-            <span>Address</span>
-            <input
-              type="text"
-              name="shop_location"
-              {...register("shop_location")}
-            />
-          </label>
-          <label className="update-form-label">
-            <span>Phone number</span>
-            <input
-              type="text"
-              name="phone_number"
-              {...register("phone_number")}
-            />
-          </label>
-          <label className="update-form-label">
-            <span>Opening time</span>
-            <input
-              type="text"
-              name="opening_time"
-              {...register("opening_time")}
-            />
-          </label>
-          <label className="update-form-label">
-            <span>Closing time</span>
-            <input
-              type="text"
-              name="closing_time"
-              {...register("closing_time")}
-            />
-          </label>
-          <button type="submit">Submit</button>
-          <p onClick={() => toggleForm()}>Cancel</p>
-        </fieldset>
-      </form>
     </>
   );
 }
