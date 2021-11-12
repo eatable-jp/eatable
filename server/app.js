fc6a07ddb03099ee977e3268bc50c56395fbfb5b
SumitVermakgp <sumitverma9628@gmail.com>
2021-11-12 15:05:41 +0900

    fix endpoints

--- server/app.js
+++ server/app.js
@@ -4,9 +4,6 @@ const path = require("path");
 const cors = require("cors")
 const knex = require('knex')
 
-//const environment = process.env.ENV || 'development';
-//const config = require("../knexfile")[environment];
-//const db = knex(config);
 
 const app = express()
 
@@ -62,12 +59,9 @@ const createUnixSocketPool = async config => {
         database: process.env.DB_NAME,
         host: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
       },
-      // ... Specify additional properties here.
       ...config,
     });
 };
-
-
 
 const createTcpPool = async config => {
     // Extract host and port from socket address
@@ -77,21 +71,17 @@ const createTcpPool = async config => {
     return knex({
       client: 'pg',
       connection: {
-        user: process.env.DB_USER, // e.g. 'my-user'
-        password: process.env.DB_PASS, // e.g. 'my-user-password'
-        database: process.env.DB_NAME, // e.g. 'my-database'
-        host: dbSocketAddr[0], // e.g. '127.0.0.1'
-        port: dbSocketAddr[1], // e.g. '5432'
+        user: process.env.DB_USER, 
+        password: process.env.DB_PASS, 
+        database: process.env.DB_NAME,
+        host: dbSocketAddr[0], 
+        port: dbSocketAddr[1],
       },
-      // ... Specify additional properties here.
       ...config,
     });
   };
 
 const createPool = async () => {
-    // Configure which instance and what database user to connect with.
-    // Remember - storing secrets in plaintext is potentially unsafe. Consider using
-    // something like https://cloud.google.com/kms/ to help keep secrets secret.
     const config = {pool: {}};
     config.pool.max = 5;
     config.pool.min = 5;
@@ -99,50 +89,20 @@ const createPool = async () => {
     config.pool.createTimeoutMillis = 30000;
     config.pool.idleTimeoutMillis = 600000;
     config.pool.createRetryIntervalMillis = 200;
-    console.log("this pool")
 
     if (process.env.DB_HOST === "127.0.0.1:8080") {
-        return createTcpPool(config);
+        return createTcpPool(config);  // for local server
     } else {
-        return createUnixSocketPool(config);
+        return createUnixSocketPool(config); // for production
     }
 };
-
-/*
-
-const ensureSchema = async pool => {
-    const hasTable = await pool.schema.hasTable('votes');
-    if (!hasTable) {
-      return pool.schema.createTable('votes', table => {
-        table.increments('vote_id').primary();
-        table.timestamp('time_cast', 30).notNullable();
-        table.specificType('candidate', 'CHAR(6)').notNullable();
-      });
-    }
-    logger.info("Ensured that table 'votes' exists");
-};
-
-
-const ensureSchema = async pool => {
-    const hasTable = await pool.schema.hasTable('items');
-    if (!hasTable) {
-      return pool.schema.createTable('items', table => {
-        table.increments('vote_id').primary();
-        table.timestamp('time_cast', 30).notNullable();
-        table.specificType('candidate', 'CHAR(6)').notNullable();
-      });
-    }
-};
-*/
 
 const createPoolAndEnsureSchema = async () =>
   await createPool()
     .then(async pool => {
-      //await ensureSchema(pool);
       return pool;
     })
     .catch(err => {
-      //logger.error(err);
       throw err;
     });
 
@@ -169,6 +129,7 @@ const getVoteCount = async (pool, candidate) => {
 
 */
 
+// Get all items
 const getItems = async pool => {
     return await pool
         .select('*')
@@ -176,14 +137,8 @@ const getItems = async pool => {
 }
 
 app.get('/items', async (req, res) => {
-    console.log("first in items  ")
-    console.log(process.env.DB_USER)
-    console.log(process.env.DB_PASS)
-    console.log(process.env.DB_NAME)
-    console.log(process.env.DB_HOST)
+
     pool = pool || (await createPoolAndEnsureSchema());
-    console.log("pool created ")
-    //console.log(pool)
     try {
         const items = await getItems(pool)
         res.json(items);
@@ -197,134 +152,244 @@ app.get('/items', async (req, res) => {
 });
 
 
-/*
-//***************************************ITEMS*************************************
-app.get('/items', async(req,res) => {
-    const items = await db.select().table("items");
-    res.json(items);
-})
+// Insert item
+const insertItem = async (pool, item) => {
+  try {
+    return await pool('items').insert(item);
+  }
+  catch (err) {
+    throw Error(err);
+  }
+}
 
-//going to be in the body JSON as is standard for post 
-app.post('/items', async(req,res) => {
-    const newItem = req.body;
-    const items = await db.select().table("items").insert(newItem);
-    res.json(`New Item ${newItem.name} has been posted to sell`)
-})
+app.post('/item', async (req, res) => {
 
-//in the body pass in the new JSON object
-app.patch('/items', async(req,res) => {
-    const c = req.body
-    const itemId = req.body.id
-    //const items = await db.select().table("items");
-    const patchItem = await db.select()
-    .table("items")
-    .where({id:itemId})
-    .update({name:c.name, image:c.image ,type:c.type,buyer_id:c.buyer_id,price:c.price,original_price:c.original_price,expiration_date:c.expiration_date,note:c.note});
-
-    res.send("item updated")
-})
-
-//grab the item with a query param
-app.delete('/items',async(req,res) => {
-    const itemId = req.query.id
-    const itemIdInt = parseInt(itemId)
-    const foundItem = await db.select().table("items").where({id:itemIdInt});
-    const deleteItem = await db.select()
-    .table("items")
-    .where({id:itemIdInt})
-    .del()
-
-    res.send(`${foundItem[0].name} has been deleted`)
-})
-
-//************************SELLERS************************************************ 
-
-app.get('/sellers', async(req,res) => {
-    const allSellers = await db.select().table("sellers")
-    res.json(allSellers)
+  pool = pool || (await createPoolAndEnsureSchema());
+  try {
+      const newItem = req.body;
+      await insertItem(pool, newItem)
+  } catch (err) {
+      console.error(err);
+  res
+    .status(500)
+    .send('Unable to add item; see logs for more details.')
+    .end();
+  }
 });
 
-//JSON body no id included when creating the DB will take care of it
-app.post('/sellers', async(req,res) => {
+
+// Update item
+const updateItem = async (pool, newItem, itemId) => {
+  try {
+    return await pool('items').where({id:itemId}).update({name:newItem.name, image:newItem.image, type:newItem.type, price:newItem.price, original_price:newItem.original_price, expiration_date:newItem.expiration_date, note:newItem.note, buyer_id:newItem.buyer_id});
+  }
+  catch (err) {
+    throw Error(err);
+  }
+}
+
+app.patch('/item', async (req, res) => {
+
+  pool = pool || (await createPoolAndEnsureSchema());
+  try {
+      const newItem = req.body;
+      const itemId = req.body.id
+      await updateItem(pool, newItem, itemId)
+  } catch (err) {
+      console.error(err);
+  res
+    .status(500)
+    .send('Unable to update item; see logs for more details.')
+    .end();
+  }
+});
+
+// Delete an item
+const deleteItem = async (pool, itemId) => {
+  try {
+    return await pool('items').where({id:itemId}).del();
+  }
+  catch (err) {
+    throw Error(err);
+  }
+}
+
+app.delete('/item', async (req, res) => {
+
+  pool = pool || (await createPoolAndEnsureSchema());
+  try {
+      const itemId = req.query.id
+      const itemIdInt = parseInt(itemId)
+      await deleteItem(pool, itemIdInt)
+  } catch (err) {
+      console.error(err);
+  res
+    .status(500)
+    .send('Unable to delete item; see logs for more details.')
+    .end();
+  }
+});
+
+
+// Get all Sellers
+const getSellers = async pool => {
+  return await pool
+      .select('*')
+      .from('sellers')
+}
+
+app.get('/sellers', async (req, res) => {
+
+  pool = pool || (await createPoolAndEnsureSchema());
+  try {
+      const sellers = await getSellers(pool)
+      res.json(sellers);
+  } catch (err) {
+      console.error(err);
+  res
+    .status(500)
+    .send('Unable to load page; see logs for more details.')
+    .end();
+  }
+});
+
+
+// Insert a seller
+const insertSeller = async (pool, seller) => {
+try {
+  return await pool('sellers').insert(seller);
+}
+catch (err) {
+  throw Error(err);
+}
+}
+
+app.post('/seller', async (req, res) => {
+
+pool = pool || (await createPoolAndEnsureSchema());
+try {
     const newSeller = req.body;
-    const s = req.body
-    const sellers = await db.select().table("sellers")
-    .insert({id: s.id,shop_name:s.shop_name, shop_location: s.shop_location, shop_long: s.shop_long,
-    shop_lat:s.shop_lat, opening_time:s.opening_time,closing_time:s.closing_time, phone_number:s.phone_number});
-    res.json(`New member named ${newSeller.name} has been created, happy selling!`)
-})
-
-app.patch('/sellers', async(req,res) => {
-    const s = req.body
-    const itemId = req.body.id
-    //const items = await db.select().table("items");
-    const patchItem = await db.select()
-    .table("sellers")
-    .where({id:itemId})
-    .update({id: s.id ,shop_name:s.shop_name, shop_location: s.shop_location, shop_long: s.shop_long,
-        shop_lat:s.shop_lat, opening_time:s.opening_time,closing_time:s.closing_time, phone_number:s.phone_number});
-
-    res.send("Seller info updated!!")
-})
-
-//Query with id
-app.delete('/sellers',async(req,res) => {
-    const itemId = req.query.id
-    const itemIdInt = parseInt(itemId)
-    const foundItem = await db.select().table("sellers").where({id:itemIdInt});
-    const deleteItem = await db.select()
-    .table("sellers")
-    .where({id:itemIdInt})
-    .del()
-
-    res.send(`${foundItem[0].shop_name} has been deleted`)
-})
-
-/************************************BUYERS************************************** 
-
-app.get("/buyers",async(req,res) => {
-    const allBuyers = await db.select().table('buyers')
-    res.json(allBuyers)
+    await insertSeller(pool, newSeller)
+} catch (err) {
+    console.error(err);
+res
+  .status(500)
+  .send('Unable to add seller; see logs for more details.')
+  .end();
+}
 });
 
-// add buyer without ID DB will assign one.
-app.post("/buyers", async(req,res) => {
-    const nB = req.body
-    const newBuyer = await db.select().table("buyers")
-    .insert({display_name:nB.display_name, email:nB.email, address: nB.address, phone_number:nB.phone_number})
-    res.json(`New user ${nB.display_name} added!`)
+
+// Update a seller
+const updateSeller = async (pool, newSeller, sellerId) => {
+try {
+  return await pool('sellers').where({id:sellerId})
+    .update({seller_name:newSeller.seller_name, shop_name:newSeller.shop_name, shop_location: newSeller.shop_location, shop_long: newSeller.shop_long,
+    shop_lat:newSeller.shop_lat, opening_time:newSeller.opening_time, closing_time:newSeller.closing_time, phone_number:newSeller.phone_number, email_address:newSeller.email_address});
+}
+catch (err) {
+  throw Error(err);
+}
+}
+
+app.patch('/seller', async (req, res) => {
+
+pool = pool || (await createPoolAndEnsureSchema());
+try {
+    const newSeller = req.body;
+    const sellerId = req.body.id
+    await updateSeller(pool, newSeller, sellerId)
+} catch (err) {
+    console.error(err);
+res
+  .status(500)
+  .send('Unable to update seller; see logs for more details.')
+  .end();
+}
 });
 
-app.patch('/buyers', async(req,res) => {
-    const nB = req.body
-    const itemId = req.body.id
-    //const items = await db.select().table("items");
-    const patchItem = await db.select()
-    .table("buyers")
-    .where({id:itemId})
-    .update({id: nB.id, display_name:nB.display_name, email:nB.email, address: nB.address, phone_number:nB.phone_number});
 
-    res.send("Buyer info updated!!")
-})
 
-//Query with ID
-app.delete('/buyers',async(req,res) => {
-    const buyerId = req.query.id
-    const buyerIdInt = parseInt(buyerId)
-    const foundBuyer = await db.select().table("buyers").where({id:buyerIdInt});
-    const deleteItem = await db.select()
-    .table("buyers")
-    .where({id:buyerIdInt})
-    .del()
 
-    res.json(`${foundBuyer[0].display_name} has been deleted`)
-})
+// Get all Buyers
+const getBuyers = async pool => {
+  return await pool
+      .select('*')
+      .from('buyers')
+}
 
-*/
+app.get('/buyers', async (req, res) => {
+
+  pool = pool || (await createPoolAndEnsureSchema());
+  try {
+      const buyers = await getBuyers(pool)
+      res.json(buyers);
+  } catch (err) {
+      console.error(err);
+  res
+    .status(500)
+    .send('Unable to load page; see logs for more details.')
+    .end();
+  }
+});
+
+// Insert a buyer
+const insertBuyer = async (pool, buyer) => {
+  try {
+    return await pool('buyers').insert(buyer);
+  }
+  catch (err) {
+    throw Error(err);
+  }
+  }
+  
+  app.post('/buyer', async (req, res) => {
+  
+  pool = pool || (await createPoolAndEnsureSchema());
+  try {
+      const newBuyer = req.body;
+      await insertBuyer(pool, newBuyer)
+  } catch (err) {
+      console.error(err);
+  res
+    .status(500)
+    .send('Unable to add buyer; see logs for more details.')
+    .end();
+  }
+  });
+  
+  
+  // Update a buyer
+  const updateBuyer = async (pool, newBuyer, buyerId) => {
+  try {
+    return await pool('buyers').where({id:buyerId})
+      .update({buyer_name:newBuyer.buyer_name, buyer_address: newBuyer.buyer_address, phone_number:newBuyer.phone_number, email_address:newBuyer.email_address});
+  }
+  catch (err) {
+    throw Error(err);
+  }
+  }
+  
+  app.patch('/buyer', async (req, res) => {
+  
+  pool = pool || (await createPoolAndEnsureSchema());
+  try {
+      const newBuyer = req.body;
+      const buyerId = req.body.id
+      await updateBuyer(pool, newBuyer, buyerId)
+  } catch (err) {
+      console.error(err);
+  res
+    .status(500)
+    .send('Unable to update buyer; see logs for more details.')
+    .end();
+  }
+  });
+
 
 //test endpoint
 app.get("/hello", async(req,res) => {
-    res.json("Let's save some food!!!!!!!!!!")
+    res.json("Let's save some food!!")
 })
 
 
