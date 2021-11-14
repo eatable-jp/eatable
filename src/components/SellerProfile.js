53d7be6417b9fa99cb3cdd5c027c5d1047bafb02
ShotaKarato <theseuta@gmail.com>
2021-11-14 15:10:39 +0900

    feat: implemented bootstrap in profile component

--- src/components/SellerProfile.js
+++ src/components/SellerProfile.js
@@ -4,37 +4,37 @@ import { useSelector } from "react-redux";
 // react router
 import { LinkContainer } from "react-router-bootstrap";
 // bootstrap
-import { Button } from "react-bootstrap";
+import { Card, Button, Container } from "react-bootstrap";
 
 export default function SellerProfile() {
   // setup redux
   const sellerInfo = useSelector((state) => state.sellerInfo);
   return (
-    <>
-      <div className="buyer-profile">
-        <h2 className="buyer-profile-name">{sellerInfo.shop_name}</h2>
-        <table className="buyer-profile-table">
-          <tbody>
-            <tr>
-              <th>Address</th>
-              <td>{sellerInfo.shop_location}</td>
-            </tr>
-            <tr>
-              <th>Business hours</th>
-              <td>
-                {sellerInfo.opening_time} - {sellerInfo.closing_time}
-              </td>
-            </tr>
-            <tr>
-              <th>Phone number</th>
-              <td>{sellerInfo.phone_number}</td>
-            </tr>
-          </tbody>
-        </table>
-        <LinkContainer to="/seller-form">
-          <Button>Edit profile</Button>
-        </LinkContainer>
-      </div>
-    </>
+    <Container className="w-50 text-center">
+      <Card>
+        <Card.Header>
+          <strong>{sellerInfo.shop_name}</strong>
+        </Card.Header>
+        <Card.Body>
+          <dl>
+            <dt>Address</dt>
+            <dd>{sellerInfo.shop_location}</dd>
+          </dl>
+          <dl>
+            <dt>Business hours</dt>
+            <dd>
+              {sellerInfo.opening_time} - {sellerInfo.closing_time}
+            </dd>
+          </dl>
+          <dl>
+            <dt>Phone number</dt>
+            <dd>{sellerInfo.phone_number}</dd>
+          </dl>
+          <LinkContainer to="/seller-form">
+            <Button variant="outline-success">Edit profile</Button>
+          </LinkContainer>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
